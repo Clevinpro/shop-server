cb53085fa549ca7931b9460e46908a7a38a8910e
Clevin <Clevin.pro@gmail.com>
2019-11-06 16:53:13 +0200

    upd

--- src/domain/db/schemas/product.js
+++ src/domain/db/schemas/product.js
@@ -2,13 +2,12 @@ const mongoose = require("mongoose");
 
 const ProductSchema = mongoose.Schema(
   {
-    id: { type: String, default: "" },
     type: { type: String, default: "" },
     gender: { type: String, default: "" },
-    material: { type: String, default: "" },
+    material: { type: Array, default: [] },
     brandName: { type: String, default: "" },
     name: { type: String, default: "" },
-    size: { type: String, default: "" },
+    size: { type: Number, default: 0 },
     image: { type: String, default: "" },
     description: { type: String, default: "" },
     descriptionFull: { type: String, default: "" },
@@ -16,9 +15,9 @@ const ProductSchema = mongoose.Schema(
     availability: { type: Number, default: 0 },
     popular: { type: Boolean, default: false },
     description: { type: String, default: "" },
-    purchases: { type: Number, default: 0 },
+    purchases: { type: Number, default: 0 }
   },
   { versionKey: false }
 );
-const Product = mongoose.model("Product", ProductSchema)
+const Product = mongoose.model("Product", ProductSchema);
 module.exports = Product;
